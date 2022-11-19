98244ac32465c76313026b336d4910b0a12fb96d
laowang <laowang@gmail.com>
2022-11-19 16:00:40 +0800

    third git

--- src/main/java/gitDemo.java
+++ src/main/java/gitDemo.java
@@ -2,5 +2,6 @@ public class gitDemo {
     public static void main(String[] args) {
         System.out.println("Hello,git!");
         System.out.println("Hello,git2!");
+        System.out.println("Hello,git2!");
     }
 }
